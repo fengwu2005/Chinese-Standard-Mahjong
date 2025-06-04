d3d0e3a053dc1c112c3be6d2e3070f014942104d
fengwu2005 <fengwu2005@hotmail.com>
2025-06-04 17:00:28 +0800

    fengwu

--- pre_train.py
+++ pre_train.py
@@ -4,9 +4,23 @@ from model import CNNModel
 import torch.nn.functional as F
 import torch
 import os
+import argparse
 
 if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter
+    )
+    parser.add_argument(
+        "--exp_name", type=str, default="test", help="Experiment name."
+    )
+    args, _ = parser.parse_known_args()
+    args = vars(args)
+    exp_name = args['exp_name']
+
     logdir = 'model/'
+    save_dir = os.path.join(logdir, exp_name)
+    os.makedirs(save_dir, exist_ok=True)
     # os.mkdir(logdir + 'checkpoint')
     
     # Load dataset
@@ -24,11 +38,13 @@ if __name__ == '__main__':
     # Train and validate
     for e in range(16):
         print('Epoch', e)
-        torch.save(model.state_dict(), logdir + 'checkpoint/%d.pkl' % e)
+        torch.save(model.state_dict(), save_dir + '/%d.pkl' % e)
         for i, d in enumerate(loader):
-            input_dict = {'is_training': True, 'obs': {'observation': d[0].cuda(), 'action_mask': d[1].cuda()}}
+            # input_dict = {'is_training': True, 'obs': {'observation': d[0].cuda(), 'action_mask': d[1].cuda()}}
+            input_dict = {'observation': d[0].cuda(), 'action_mask': d[1].cuda()}
             # print(input_dict["obs"]["observation"].shape())
-            logits = model(input_dict)
+            logits, _ = model(input_dict)
+            # print(logits.shape, d[2].long().cuda().shape)
             loss = F.cross_entropy(logits, d[2].long().cuda())
             if i % 8 == 0:
                 print('Iteration %d/%d'%(i, len(trainDataset) // batchSize + 1), 'policy_loss', loss.item())
@@ -39,7 +55,8 @@ if __name__ == '__main__':
         print('Run validation:')
         correct = 0
         for i, d in enumerate(vloader):
-            input_dict = {'is_training': False, 'obs': {'observation': d[0].cuda(), 'action_mask': d[1].cuda()}}
+            # input_dict = {'is_training': False, 'obs': {'observation': d[0].cuda(), 'action_mask': d[1].cuda()}}
+            input_dict = {'observation': d[0].cuda(), 'action_mask': d[1].cuda()}
             with torch.no_grad():
                 logits, _ = model(input_dict)
                 pred = logits.argmax(dim = 1)
